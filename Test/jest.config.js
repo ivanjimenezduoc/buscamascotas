1ca9f44ab54726a4d36a3c60440b42b52db8305c
xBitoo <102562430+xBitoo@users.noreply.github.com>
2024-09-26 02:20:40 -0300

    integracionbabel2

--- Test/jest.config.js
+++ Test/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     testEnvironment: 'jsdom', // Asegura que Jest use un entorno DOM para pruebas de front-end
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Para cargar las extensiones de Testing Library
+    transform: {'^.+\\.(js|jsx)$': 'babel-jest',}
   };
   
