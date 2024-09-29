68d2f0499a144a3423a3ba26101ce5c58b9f1f4c
xBitoo <102562430+xBitoo@users.noreply.github.com>
2024-09-29 02:47:12 -0300

    integrandoTesting

---
+++ Test/test2.test.js
@@ -0,0 +1,37 @@
+import { screen, render } from '@testing-library/dom';
+import fs from 'fs';
+import path from 'path';
+
+describe('Pruebas del HTML de BuscaMascotas', () => {
+  let html;
+
+  beforeEach(() => {
+    // Lee el archivo HTML y almacénalo en una variable
+    const filePath = path.resolve('src/index.html'); // Ajusta la ruta según la ubicación de tu archivo HTML
+    html = fs.readFileSync(filePath, 'utf-8');
+  });
+
+  test('verifica que el título sea BuscaMascotas', () => {
+    document.body.innerHTML = html; // Inyecta el HTML en el DOM
+    const title = document.querySelector('title').innerHTML; // Obtiene el título
+    expect(title).toBe('BuscaMascotas'); // Verifica el título
+  });
+
+  test('verifica que el encabezado contenga el texto correcto', () => {
+    document.body.innerHTML = html; // Inyecta el HTML en el DOM
+    const headerText = screen.getByRole('heading', { name: /BuscaMascotas/i });
+    expect(headerText).toBeInTheDocument(); // Verifica que el encabezado esté en el documento
+  });
+
+  test('verifica que haya un enlace a "Inicio"', () => {
+    document.body.innerHTML = html; // Inyecta el HTML en el DOM
+    const homeLink = screen.getByText(/Inicio/i); // Busca el enlace
+    expect(homeLink).toBeInTheDocument(); // Verifica que el enlace esté en el documento
+  });
+  test('verifica que haya un enlace a "Agregar Mascota"', () => {
+    document.body.innerHTML = html; // Inyecta el HTML en el DOM
+    const homeLink = screen.getByText(/Agregar Mascota/i); // Busca el enlace
+    expect(homeLink).toBeInTheDocument(); // Verifica que el enlace esté en el documento
+  });
+
+});
